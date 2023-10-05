51b3ef80259d37ecb10487ace8eefb1075aee738
Alexandru Nedelcu <noreply@alexn.org>
2023-10-05 11:20:02 +0300

    Update versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,5 +9,5 @@ pluginManagement {
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.51.0"
+    id("de.fayard.refreshVersions") version "0.60.3"
 }
